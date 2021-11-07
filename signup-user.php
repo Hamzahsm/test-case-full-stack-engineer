5ea13040d516ff2c214f9f3d968197743f3f35f1
Hamzahsm <hamzahsaifulmadjid@gmail.com>
2021-11-08 00:06:22 +0700

    set up file

---
+++ signup-user.php
@@ -0,0 +1,91 @@
+<?php require_once "controllerUserData.php"; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Signup Form</title>
+  </head>
+  <body style="background-color: #570000;">
+
+<div class="container" style="margin-top: 30px;">
+    <div class="col-8" style="margin: auto;">
+        <form style="background-color: white; padding: 20px; border-radius: 10px;" action="signup-user.php" method="POST">
+        <div style="text-align: center; margin-bottom: 20px;">
+              <img src="https://oesman.id/wp-content/uploads/2020/08/OESMAN_logo-4.png" width="240px" height="80px" class="img-fluid">
+              </div>
+              <h2 class="text-center">Registration</h2>
+              <?php
+                    if(count($errors) == 1){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php
+                            foreach($errors as $showerror){
+                                echo $showerror;
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }elseif(count($errors) > 1){
+                        ?>
+                        <div class="alert alert-danger">
+                            <?php
+                            foreach($errors as $showerror){
+                                ?>
+                                <li><?php echo $showerror; ?></li>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="username" placeholder="Username" required value="<?php echo $username ?>">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm password" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="text" name="name" placeholder="Full Name" required value="<?php echo $name ?>">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="email" name="email" placeholder="Email Address" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="number" name="nomortelepon" placeholder="Nomor Telepon" required value="<?php echo $nomortelepon ?>">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input class="form-control" type="text" name="namausaha" placeholder="Nama Usaha" required value="<?php echo $namausaha ?>">
+                    </div>
+
+                    <div class="form-group mt-3">
+                        <input class="form-control button" type="submit" name="signup" value="Signup">
+                    </div>
+                    <div class="link login-link text-center mt-3">Already a member? <a href="index.php">Login here</a></div>
+        </form>
+    </div>
+</div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
